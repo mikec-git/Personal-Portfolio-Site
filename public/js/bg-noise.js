0353486fde5f35f7c1c22145e2c6a274ea2da5c5
Mike Choi <mike.choi@live.ca>
2018-10-14 16:57:29 -0700

    Added loop function and generating noise image data functions for bg-noise

--- public/js/bg-noise.js
+++ public/js/bg-noise.js
@@ -5,9 +5,12 @@
     let canvas,
         ctx,
         windowWidth,
-        windowHeight,
-        idata;
+        windowHeight;
     
+    // Counter for looping over noiseData
+    let imageFrame = 0;
+    let numOfFrames = 10;
+    // Array to keep numOfFrames number of rendered images
     let noiseData = [];
     
     function init(e) {
@@ -15,7 +18,7 @@
         
         canvas = document.querySelector('.noise');
         ctx = canvas.getContext('2d', {alpha: true});
-        
+
         setupCanvas();
     }
     
@@ -23,16 +26,46 @@
         windowWidth = document.documentElement.clientWidth;
         windowHeight = document.documentElement.clientHeight;
 
-        idata = ctx.createImageData(windowWidth, windowHeight);
-        buffer32 = new Uint32Array(idata.data.buffer);
+        canvas.width = windowWidth;
+        canvas.height = windowHeight;
 
+        generateImageDataArray();
+    }
+    
+    function generateImageDataArray() {
+        // Prerender images and store into array
+        for(let i = 0; i < numOfFrames; i++) {
+            generateData();
+        }
+        
+        runLoop();
+    }
+    
+    function generateData() {
+        // Create the image data object
+        let idata = ctx.createImageData(windowWidth, windowHeight);
+        // Initialized view with all values of 0
+        buffer32 = new Uint32Array(idata.data.buffer);
         let length = buffer32.length;
+        
         for(let i = 0; i < length; i++) {
+            // Half of the data, at random, will be opaque black
             if(Math.random() < 0.5) {
                 buffer32[i] = 0xff000000;
             }
         }
-
+        
         noiseData.push[idata];
     }
+
+    function runLoop() {
+        imageFrame = (imageFrame >= numOfFrames) ? 0 : imageFrame++;
+
+        ctx.putImageData(noiseData[imageFrame], 0, 0);
+
+        requestAnimationFrame(runLoop);
+    }
+
+
+
 })();
